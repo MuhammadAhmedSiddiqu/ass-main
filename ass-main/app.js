e0a3704d4d4ac81b15b0d61270a762275c8825f8
MuhammadAhmedSiddiqu <ahmedsiddiqui461@gmail.com>
2024-02-29 09:53:46 +0500

    all files

---
+++ ass-main/app.js
@@ -0,0 +1,92 @@
+// Chapter # 01
+
+// quesion # 01
+
+// alert ("")
+
+// quesion # 02
+
+// alert ("Error! Please enter a valid password.")
+
+// quesion # 03
+
+// alert ("Welcome to JS land \n Happy Coding!")
+
+// quesion # 04
+
+// alert ("Welcome to JS land")
+// alert ("Happy Coding!")
+
+// quesion # 05
+
+// console.log ("Hello... ! I can run JS through my web brower's console")
+
+
+
+// quesion # 06
+// DONE 
+// quesion # 07
+// done
+
+//chapter #02
+
+// question #01
+
+// var username ="arreb";
+// alert(username);
+
+//question #02;
+// var myname ="Syed Muhammad Areeb Zaidi"
+// alert (myname);
+
+// //question #03;
+// var message = "Hello world";
+// alert (message);
+
+// //question #04;
+// var name="jhon doe";
+// alert(name);
+
+// var age="15 year old";
+// alert(age);
+
+// var certificate=" Certified Mobile Application Development";
+// alert(certificate);
+
+// //question #05;
+// var pizza =" PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza)
+
+// question #06;
+// var email =" example@ example.com";
+// alert( "My email address is "+email);
+
+// question #07;
+// var book ="A Smarter Way TO Learn Java script"
+// alert("I am tring to learn from Book"+book)
+
+// question #08;
+// document.write("Yah! i can write HTML content though JAVA SCRIPT")
+
+// question #09;
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ")
+
+//chapter #03
+
+// question #01;
+// var age ="i am 15 year old"
+// alert(age)
+
+//question #02;
+// var no =" 14";
+// alert("you have visited this site"+no +" times")
+
+//question #03;
+// var birth =" 2006"
+// document.write ("My Birth year is"+ birth + "<br> Data type of my ddcleared variable is number")
+
+//question #04;
+// var visitorname ="Jhon Doe "
+// var product ="5 T-Shirt"
+
+// document.write("<b>"+visitorname+"</b>"+"ordered "+"<b>"+product+"</b>"+"(s)"+"on XYZ Clothing store");
